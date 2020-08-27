7dd7bc554c88442ef489ae19eb26b886b394b47e
Flunt1k <chvp00@gmail.com>
2020-08-27 11:35:34 +0300

    main build

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const path = require('path');
-const mongoose = require('mongoose');
+
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './views'));
 
 app.get('/', (req, res) => {
   res.render('index');
