fd5fa3a888cde1b20c99852293d6af542b573935
Pavel <b27394@gmail.com>
2020-08-27 13:08:15 +0300

    Start

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const middleWare = require('./middleware/index');
+
+middleWare(app);
+
 app.get('/', (req, res) => {
   res.render('index');
 });
--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,16 @@
-module.exports = function(app) {
+module.exports = function middleWare(app) {
   const express = require('express')
   const morgan = require('morgan');
   const cookieParser = require('cookie-parser');
   const path = require('path');
   const mongoose = require('mongoose');
 
-
   app.use(morgan('dev'));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, '..','./public')));
+  app.use(express.static(path.join(__dirname, '../public')));
 
   app.set('view engine', 'hbs');
-  app.set('views', path.join(__dirname, '..' ,'./views'));
-}
+  app.set('views', path.join(__dirname, '../views'));
+};
