e620d2ab6ebe359002bba760d7201da342bcbc9a
Pavel <b27394@gmail.com>
2020-08-27 20:35:14 +0300

    pagination release 1

--- routes/basicRouts.js
+++ routes/basicRouts.js
@@ -6,14 +6,19 @@ const Vacancy = require('../models/Vacancy');
 router.get('/', async (req, res) => {
   const limit = 5;
   const startInd = limit * (req.query.page - 1);
+  const endInd = startInd + limit;
+  let next = +req.query.page + 1;
+  let prev = +req.query.page - 1;
   const arr = await Vacancy.find({}).limit(32);
-  const onePage = arr.slice(startInd, startInd + limit);
+  const onePage = arr.slice(startInd, endInd);
   const total = Math.ceil(arr.length / limit);
+  if (prev <= 0) prev = false;
+  if (next > total) next = false;
   const pages = [];
   for (let i = 1; i <= total; i += 1) {
     pages.push({ num: i });
   }
-  res.render('index', { onePage, pages });
+  res.render('index', { onePage, pages, prev, next });
 });
 
 // router.get('/logout', (req, res) => {
